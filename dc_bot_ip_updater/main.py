dcedd19aafe086dcc150c2b5abed3dfcfbed6df7
Szabo Istvan <pisti4395@gmail.com>
2025-04-02 18:07:36 +0200

    update

---
+++ dc_bot_ip_updater/main.py
@@ -0,0 +1,43 @@
+import discord
+import requests
+import subprocess
+import os
+
+TOKEN = "MTA4MzEyMDg3MDUwMDg4MDUxNA.GW1ZcX.STs0V4FmgdBmKtd7hXmi-nzHslfk-6MKXlxciQ"
+intents=discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+#client = discord.Client()
+apikey = "6a0453054a324405896fd9b577e4b1da"
+
+@client.event
+async def on_ready():
+    pass
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "ip":
+        ip = str(subprocess.run(["curl", "ifconfig.co"], capture_output=True).stdout).strip()
+        ip = ip[2:-3]
+        mullvad = str(subprocess.run(["curl", "https://am.i.mullvad.net/ip"], capture_output=True).stdout.strip())
+        mullvad = mullvad[2:-1]
+        await message.channel.send(f"ip: {ip}\nmullvad ip{str(mullvad)}\njellyfin link: http://{ip}:8096\njellyfin local: http://192.168.8.121:8096")
+    if message.content[0:3] == "new" or message.content[0:3] == "New" :
+        try:
+            name = message.content.split(" ")[1]
+            passw = message.content.split(" ")[2]
+            headers = {
+                "X-Emby-Token": apikey}
+            payload = {
+                "Name" : name,
+                "Password" :passw
+            }
+            response = requests.post("http://localhost:8096/Users/New", headers=headers, json=payload)
+            print(response.status_code)
+            print(response.json())
+            await message.channel.send(f"created new account {name} {passw}")
+        except:
+            await message.channel.send("error")
+client.run(TOKEN)
---
+++ dc_bot_ip_updater/run.sh
@@ -0,0 +1,2 @@
+#!bash
+/home/istipisti113/dc_bot_ip_updater/bin/python3 /home/istipisti113/dc_bot_ip_updater/main.py
